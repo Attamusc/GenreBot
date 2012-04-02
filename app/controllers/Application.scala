ee47a835d4188384d491085ada60a72e90512575
Sean Dunn <sean@attamusc.com>
2012-04-01 23:46:33 -0400

    Cool turntables :)

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,28 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.libs.Files
+
+import java.io.File
 
 object Application extends Controller {
   
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index())
+  }
+
+  def about = Action {
+    Ok(views.html.about())
+  }
+
+  def genreParse = Action(parse.multipartFormData) { request =>
+    request.body.files.map { file =>
+      val path = "/tmp/" 
+      val filename = path + file.filename
+      Files.copyFile(file.ref.file, new File(filename)) 
+    }
+
+    Ok("File Uploaded")
   }
   
 }
---
+++ public/javascripts/main.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+    $(".vinyl-text").arctext({ radius: 120 });
+});
