3773335c4e3ce4c379be8e2476cea82e546a1971
Sean Dunn <sean@attamusc.com>
2012-05-06 01:29:32 -0400

    Added in a results page and styled it to show size based on percentage present

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,34 +3,38 @@ package controllers
 import play.api._
 import play.api.mvc._
 import play.api.libs.Files
-import play.api.libs.json
-
+import play.api.libs.json._
 import java.io.File
-
 import dispatch._
+import norsys.netica._
 
 object Application extends Controller {
-  
+
   def index = Action {
     Ok(views.html.index())
   }
 
   def genreParse = Action(parse.multipartFormData) { request =>
+    var rock_percentage : Float = 0.0f
+    var electronic_percentage : Float = 0.0f
+
+    // Grab files from our request
     request.body.files.map { file =>
       val path = "/tmp/" 
-      val filename = path + file.filename
+      val filename = path + file.filename.replaceAll("""\s+""", "-")
       Files.copyFile(file.ref.file, new File(filename))
       
       // Make a remote request to our tornado server
       val h = new Http
-      val resp = h(url("http://localhost:8888/parse?filename=" + file.filename) as_str)
+      val resp = h(url("http://localhost:8888/parse?filename=" + filename) as_str)
 
-      //val json_test = Json.parse(resp)
+      val json_resp = Json.parse(resp)
 
-      Logger.info(resp)
+      rock_percentage = (json_resp \ "rock").as[Float]
+      electronic_percentage = (json_resp \ "electronic").as[Float]
     }
 
-    Ok("File Uploaded")
+    Ok(views.html.result(rock_percentage, electronic_percentage))
   }
   
 }
